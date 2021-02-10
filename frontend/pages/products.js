41db7a1118dfc80dc67f65a08f84cb0d29687495
Martian <martian2lee@163.com>
2021-02-10 13:37:38 +0800

    #32 — Pagination Links

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,9 +1,12 @@
 import Products from '../components/Products';
+import Pagination from '../components/Pagination';
 
 export default function OrderPage() {
   return (
     <div>
+      <Pagination page={2} />
       <Products />
+      <Pagination page={2} />
     </div>
   );
 }
